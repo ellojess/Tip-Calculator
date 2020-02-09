8fa2499dd65372607824e5fc66377c72ffcb79c4
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-02-08 16:59:29 -0800

    add ColorTheme data structure

---
+++ TipCalculatorStarter/ColorTheme.swift
@@ -0,0 +1,26 @@
+//
+//  ColorTheme.swift
+//  TipCalculatorStarter
+//
+//  Created by Bo on 2/8/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColorTheme {
+
+    // MARK: - Instance Vars
+
+    let isDefaultStatusBar: Bool
+    let viewControllerBackgroundColor: UIColor
+
+    let primaryColor: UIColor
+    let primaryTextColor: UIColor
+
+    let secondaryColor: UIColor
+
+    let accentColor: UIColor
+    let outputTextColor: UIColor
+}
